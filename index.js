0fc367fc4798f4d74936babe4e4e07502fc28999
magodi20 <manolo.2000@outlook.es>
2020-07-03 22:01:19 -0500

    First commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const app = express()
 const bodyParser = require("body-parser")
 
-app.use(bodyParser.json({ type: 'application/*+json' }))
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
@@ -13,27 +13,38 @@ class Task {
       this.description = description;
       this._id = Math.floor(Math.random() * 100)
     }
+
+    get id(){
+      return this._id
+    }
 };
 
-const tasks = []
+const tasks = [{name: "study", description: "programming", id: 55}, 
+{name: "exercice", description: "fausto", id: 87}]
 
 app.get("/", (req, res) => {
   res.json(tasks)
 })
 
 
-app.get("/:name", (req, res) => {
-  const foundtask = req.params.name
-  res.send(foundtask)
-})
-
-app.post('/tasks', (req, res) => {
+app.get("/:id", (req, res) => {
+  // TODO: Implement get task by id, wit error if task is not found. 
+  console.log(req.params.id)
+  const foundId = tasks.find(element =>
+    element.id === req.params.id)
+ 
+  res.json(foundId)
+  })
+app.post('/', (req, res) => {
   const t = new Task(req.body.name, req.body.description)
   tasks.push(t)
-  res.send(t)
-  console.log(req.body.name)
+  //Implement proper response (Succesfully created, and id ) 
+  console.log(t)
 })
-
+// Implement delete (by id in path)  functionality
+app.delete('/:id', (req, res) => {
+  const element = req.body.id
+ });
 
 app.listen(3000, () => {
     console.log(`Express listening on port 3000`)
