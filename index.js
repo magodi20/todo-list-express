0f08a2ca026a53803f4ae53ca7d7951809468b51
magodi20 <manolo.2000@outlook.es>
2020-07-07 19:51:45 -0500

    Get, Post and Delete requests built

--- index.js
+++ index.js
@@ -5,18 +5,17 @@ const bodyParser = require("body-parser")
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
 
 
 class Task {
     constructor(name, description) {
       this.name = name;
       this.description = description;
-      this._id = Math.floor(Math.random() * 100)
+      this.id = Math.floor(Math.random() * 100)
     }
 
-    get id(){
-      return this._id
-    }
+    
 };
 
 const tasks = [{name: "study", description: "programming", id: 55}, 
@@ -29,21 +28,34 @@ app.get("/", (req, res) => {
 
 app.get("/:id", (req, res) => {
   // TODO: Implement get task by id, wit error if task is not found. 
-  console.log(req.params.id)
-  const foundId = tasks.find(element =>
-    element.id === req.params.id)
- 
-  res.json(foundId)
+  const foundId = tasks.find(element => {
+  return element.id == req.params.id 
+  })
+  if (foundId) {
+    res.json(foundId) 
+  } else {
+    res.json(`Error, Task # ${req.params.id} was not found`)
+  }
+
   })
 app.post('/', (req, res) => {
   const t = new Task(req.body.name, req.body.description)
   tasks.push(t)
   //Implement proper response (Succesfully created, and id ) 
-  console.log(t)
+ 
+ res.json("Succesfully created task with new id:" + tasks[tasks.length - 1].id)
+ 
 })
 // Implement delete (by id in path)  functionality
 app.delete('/:id', (req, res) => {
-  const element = req.body.id
+  idDelete = req.params.id
+  for( let i = 0; i < tasks.length; i++) {
+    if ( tasks[i].id == idDelete) {
+      tasks.splice(i, 1)
+    }
+   }
+  
+   res.json(tasks)
  });
 
 app.listen(3000, () => {
